b92a3508bf97b37b9202e1f4ab3918f112acc02c
Vincent Tran <vincentuong@yahoo.com>
2021-06-10 02:39:57 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,22 @@
-require 'pry'
+
 
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    arr = self.split(/[!.?]+/)
+    arr.length
+  end 
 
-  end
 end
